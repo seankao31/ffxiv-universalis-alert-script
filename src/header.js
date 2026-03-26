f2cc6271ed5e49106a61eeee09a02f9445eed468
Sean Kao <seankao31@gmail.com>
2026-03-26 16:47:23 +0800

    chore: update userscript header with author, homepage, and pre-release version

--- src/header.js
+++ src/header.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Universalis Alert Manager
 // @namespace    https://universalis.app/
-// @version      1.0.0
+// @version      0.1.0
 // @description  Multi-world bulk alert creation and management for Universalis
-// @author       You
+// @author       Yshan
+// @homepage     https://yhkao.com
 // @match        https://universalis.app/*
 // @grant        GM_getValue
 // @grant        GM_setValue
